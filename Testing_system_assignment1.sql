8e04daa604582c9d17505491da5ba96966b1d6aa
= <=>
2023-10-25 13:50:17 +0700

    push p2

--- Testing_system_assignment1.sql
+++ Testing_system_assignment1.sql
@@ -79,3 +79,103 @@ constraint FK_exID foreign key(examID) references exam(examID)
 -- alter table examquestion add constraint FK_qsID FOREIGN KEY(questionID) references Question(questionID)
 -- alter table examquestion drop constraint FK_qsID
 -- insert into data 
+insert into department (departmentName) values ('training'),( 'sale'), ('marketing'), ('accounting'), ('human resources');
+insert into Position1 (PositionName) values ('Dev'), ('Tester'), ('Designer'), ('PM'), ('BA')
+insert into Account (Email, Username, FullName, DepartmentID, PositionID, CreateDate) values
+('dev@gmail.com', 'haianh', 'Nguyễn Hải Anh', 1, 1, '2023-01-12'),
+('tester@gmail.com', 'thaonguyen', 'Đỗ Thảo Nguyên', 1, 2, '2023-12-13'),
+('designer@gmail.com', 'thienphuc', 'Nguyễn Thiên Phúc', 3, 3, '2023-10-01'),
+('itmee@gmail.com', 'kimanh', 'Trần Kim Anh', 4, 5, '2023-12-01'),
+('windy@gmail.com', 'hauu', 'Nguyễn Thuý Hậu', 5, 4, '2023-12-10')
+
+-- delete from Account where AccountID = 2
+select *from question;
+insert into groupp(groupName, creatorID, createdate) values 
+('group_dev', 'CID01', '2023-09-12'),
+('group_tester', 'CID02', '2023-01-02'),
+('group_design', 'CID03', '2022-01-02'),
+('group_pm', 'CID04', '2023-08-09'),
+('group_ba', 'CID05', '2021-08-09') 
+-- viết trigger để check date 2 bảng trước khi insert
+insert into groupaccount values
+(1,1,'2023-09-15'), 
+(2,3, '2023-09-13'),
+(3,4, '2022-12-14'),
+(4,5, '2023-10-15'),
+(5,2, '2021-08-16')
+
+insert into typequestion(typename) values ('essay'), ('multiple-choice'), ('interview'), ('practice');
+insert into categoryquestion(categoryname) values ('Java'), ('.Net'), ('SQL'), ('Postman'), ('Ruby')
+insert into question(content, categoryID, typeID, creatorID, createdate) values
+('Thế nào là lập trình hướng đối tượng', 1, 1, 'CIDQ01', '2023-10-20'),
+('Ngôn ngữ nào mà .NET hỗ trợ', 2,2, 'CIDQ02', '2023-10-21'),
+('DML và DDL là gì', 3, 3, 'CIDQ03', '2023-10-22'),
+('Postman là gì', 4, 4, 'CIDQ04','2023-10-23');
+
+alter table answer modify column content varchar(200)
+insert into answer(content, questionID, iscorrect) VALUES
+('Ruby On rails là một Framework cho phép phát triển ứng dụng Web', 4, 'true'),
+('DDL là : create. DML là(data manipulation L) ngôn ngữ định nghĩa dữ liệu: create, alter, drop, rename', 3, 'true'),
+('Lập trình hướng đối tượng là phương pháp lập trình dựa trên lớp và đối tượng', 1, 'true'),
+('Ngôn ngữ mà .NET hỗ trợ là C#, F#,..', 2, 'true');
+
+insert into exam(code, title, categoryID, duration, creatorID, createdate) values 
+('Test1', 'Bài kiểm tra giữa kì', 1, '00:45:00', 'CIDE01', '2021-10-26'),
+('Test2', 'Bài kiểm tra khảo sát', 2, '00:30:00', 'CIDE02',  '2021-10-27'),
+('Test3', 'Bài kiểm tra cuối kì', 3, '00:10:00', 'CIDE03', '2021-10-28'),
+('Test4', 'Phỏng vấn nhanh', 4, '00:10:00', 'CIDE04', '2021-10-29'),
+('Test5', 'Thực hành', 5, '00:40:00', 'CIDE05', '2021-10-30')
+alter table exam auto_increment=1;
+ select *from exam where examID=1
+insert into examquestion values (1,1), (2,2), (3,3), (4,4)
+select *from examquestion
+====
+insert into Department (DepartmentName) values ('Audit'), ('Administration'), ('Customer services'), ('Financial') , ('Research and development') 
+insert into position1(PositionName) values ('Manager'), ('President'), ('Director'), ('Secretary'), ('Receptionist')
+insert into account (Email, Username, FullName, DepartmentID, PositionID, CreateDate) values
+('person1@gmail.com', 'rihana123', 'Rihana_Dimonds', 2, 7, '2020-01-14'),
+('person2@gmail.com', 'adele345', 'Adele', 7, 8, '2021-12-10' ),
+('person3@gmail.com', 'beyonce567', 'Beyonce', 1, 9, '2020-09-18' ),
+('person4@gmail.com', 'taylor789', 'Taylor Swift', 3, 10, '2002-09-18' ),
+('person5@gmail.com', 'andrea', 'Andera Aybar', 5, 6, '2010-10-10' )
+insert into groupp (groupName, creatorID, createdate) values 
+('group_sale', 'CID06', '2023-09-15'),
+('group_7', 'CID07', '2023-10-02'),
+('group_8', 'CID08', '2022-10-20'),
+('group_9', 'CID09', '2023-10-20'),
+('group_10', 'CID010', '2022-08-09')
+insert into groupaccount values
+(6,1,'2023-02-12'), 
+(7,10, '2023-10-20'),
+(8,7, '2023-01-14'),
+(9,6, '2023-10-21'),
+(10,8, '2022-12-16')
+update groupaccount set joindate = '2022-12-16' where groupID='10'
+select *from groupaccount
+insert into typequestion(typename) values ('type5'), ('type6'),('type7'), ('type8'), ('type9'), ('type10')
+insert into categoryquestion(categoryname) values ('Android'), ('C++'), ('C#'), ('VB'), ('Python')
+insert into question(content, categoryID, typeID, creatorID, createdate) values
+('Câu hỏi: Thế nào là 1 object?', 8, 2, 'CIDQ01', '2022-10-20'),
+('Con trỏ trong C++ là gì?', 7,3, 'CIDQ05', '2009-10-21'),
+('PEP 8 là gì', 10, 10, 'CIDQ04', '2023-10-10'),
+('Vòng đời Activity là gì', 6, 4, 'CIDQ03','2023-10-01'),
+('Có mấy loại Intent', 6, 1, 'CIDQ02','2013-12-12'),
+('Namespaces trong Python là gì', 10, 2, 'CIDQ02','2019-12-12')
+
+insert into answer(content, questionID, iscorrect) VALUES
+('Lập trình hướng đối tườn-OOP được viết tắt là Object Oriented Programming', 1, 'true'),
+('Object là 1 thực thể cụ thể của 1 class, nó bao gồm các thuộc tính và phương thức', 5, 'true'),
+('Con trỏ(pointer) là 1 biến dùng để lưu trữ địa chỉ của 1 biên khác', 6, 'true'),
+('là tập hợp các chỉ dẫn về định dạng code, phong cách lập trình được chia sẻ chung giữa các lập trình viên Python', 7, 'true'),
+('Vòng đời activity được nhắc đến để thể hiện 1 chuỗi hoạt động của 1 ứng dụng trong android theo thứ tự của các callback: onCreate, onStart, onResume', 8, 'true'),
+('Lập trình hướng đối tượng có 4 tính chất: kế thừa, đóng gói, đa hình, trừu tượng', 1, 'true'),
+('Lập trình hướng đối tượng gồm 2 thành phần là  method và attributes', 1, 'true')
+
+insert into exam(code, title, categoryID, duration, creatorID, createdate) values 
+('Test6', 'Bài kiểm tra giữa kì', 2, '00:45:00', 'CIDE01', '2022-04-19'),
+('Test7', 'Bài kiểm tra khảo sát', 7, '00:30:00', 'CIDE02',  '2021-10-27'),
+('Test8', 'Bài kiểm tra cuối kì', 8, '01:30:00', 'CIDE04', '2018-10-28'),
+('Test9', 'Phỏng vấn nhanh', 9, '00:10:00', 'CIDE05', '2021-10-29'),
+('Test10', 'Thực hành', 5, '00:25:00', 'CIDE05', '2016-10-30')
+select *from examquestion
+insert into examquestion values (1,5), (1,6), (2,9), (2,10), (6,9), (7, 1), (10, 4), (3,8), (5,4)
